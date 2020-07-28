069b625b0c8c80ec77807bce0ce419ec9cf56d1d
xack20 <zakariahossain20@gmail.com>
2020-07-29 02:30:19 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,41 +1,36 @@
 var xmin,xmax,ymin,ymax;
 var x1,y1,x2,y2;
 
-
-function check(x, y){
-
-  var v = "" ;
-  v = (y > ymax ) ? v.concat("1") : v.concat("0");
-  v = (y < ymin) ? v.concat("1") : v.concat("0");
-  v = (x > xmax) ? v.concat("1") : v.concat("0");
-  v = (x < xmin) ? v.concat("1") : v.concat("0");
-
-  return v;
-}
-
 function calc(){
 
-    xmin = document.getElementById('xmin').value;
-    xmax = document.getElementById('xmax').value;
-    ymin = document.getElementById('ymin').value;
-    ymax = document.getElementById('ymax').value;
+    xmin = Number(document.getElementById('xmin').value);
+    xmax = Number(document.getElementById('xmax').value);
+    ymin = Number(document.getElementById('ymin').value);
+    ymax = Number(document.getElementById('ymax').value);
 
-    x1 = document.getElementById('x1').value;
-    y1 = document.getElementById('y1').value;
-    x2 = document.getElementById('x2').value;
-    y2 = document.getElementById('y2').value;
+    x1 = Number(document.getElementById('x1').value);
+    y1 = Number(document.getElementById('y1').value);
+    x2 = Number(document.getElementById('x2').value);
+    y2 = Number(document.getElementById('y2').value);
 
   var p1 = check(x1,y1) , p2 = check(x2,y2);
-
-  if(p1 == "0000" && p2 == "0000")
-
 
 
   var show = "The region code for ( "+x1+", "+y1+" ) is "+check(x1,y1)+"<br>"   +
              "The region code for ( "+x2+", "+y2+" ) is "+check(x2,y2)+"<br>";
 
-
+  if(p1 == "0000" && p2 == "0000")show+="<br><br><br>The line is <b>visible</b> because both region code is <b>0000</b><br>";
+  //else if(p1&p2)
 
   document.getElementById('l1').innerHTML = show;
 
+}
+
+function check(x, y){
+  var v = "" ;
+  v = (y > ymax ) ? v.concat("1") : v.concat("0");
+  v = (y < ymin) ? v.concat("1") : v.concat("0");
+  v = (x > xmax) ? v.concat("1") : v.concat("0");
+  v = v.concat( (x < xmin) ? '1' : '0');
+  return v;
 }
